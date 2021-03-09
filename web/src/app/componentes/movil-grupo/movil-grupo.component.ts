838ada0d09de29aec95bbbc7b2f506021e40ef41
carolinnasott <carolinnasottelo@gmail.com>
2021-03-09 11:59:55 -0300

    movil-grupo

--- web/src/app/componentes/movil-grupo/movil-grupo.component.ts
+++ web/src/app/componentes/movil-grupo/movil-grupo.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { ConfirmarComponent } from '../../shared/confirmar/confirmar.component';
+import { MovilGrupo } from '../../modelo/movil-grupo';
+import { MovilGrupoService } from '../../servicios/movil-grupo.service';
+import { Grupo } from '../../modelo/grupo';
+import { GrupoService } from '../../servicios/grupo.service';
 
 @Component({
   selector: 'app-movil-grupo',
@@ -7,9 +16,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovilGrupoComponent implements OnInit {
 
-  constructor() { }
+  @Input() moviId= 0;
+
+  movilgrupos: MovilGrupo[] = []
+  seleccionado = new MovilGrupo();
+
+  columnas: string[] = ['grupNombre','acciones'];
+  dataSource = new MatTableDataSource<MovilGrupo>();
+
+  form = new FormGroup({});
+
+  mostrarFormulario = false;
+  grupos: Grupo[] = [];
+
+  constructor(
+    private movilgrupoService: MovilGrupoService,
+    private grupoService: GrupoService,
+    private formBouilder: FormBuilder,
+    private matDialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBouilder.group({
+      mogrId: [''],
+      mogrMoviId: [''],
+      mogrGrupId: [''],
+      mogrFechaAlta: [''],
+      mogrBorrado: [''],
+      grupNombre: ['']
+
+    });
+
+    this.movilgrupoService.get(`mogrMoviId=${this.moviId}`).subscribe(
+      (movil) => {
+        this.movilgrupos = movil;
+        this.actualizarTabla();
+      }
+    );
+
+    this.grupoService.get().subscribe(
+      (grupo) => {
+        this.grupos = grupo;
+      }
+    );
   }
+
+  actualizarTabla() {
+    this.dataSource.data = this.movilgrupos;
+  }
+
+  filter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  agregar() {
+    
+  }
+
+  delete(row: MovilGrupo) {
+
+  }
+
+  edit(seleccionado: MovilGrupo) {
+   
+  }
+
+  guardar() {
+
+  }
+
+  cancelar() {
+    this.mostrarFormulario = false;
+  }
+
+ 
+
 
 }
